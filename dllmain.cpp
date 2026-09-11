e60cc7442f8f0ba30494d2fd803885523e326561
diversionka402-ship-it <diversionka402@gmail.com>
2026-09-11 13:18:52 +0300

    Update dllmain.cpp

--- dllmain.cpp
+++ dllmain.cpp
@@ -1,8 +1,18 @@
 #include <windows.h>
+#include "CMenuManager.h"
 
 BOOL APIENTRY DllMain(HMODULE hModule, DWORD reason, LPVOID) {
     if (reason == DLL_PROCESS_ATTACH) {
-        MessageBoxA(NULL, "ASI zagruzilsya uspeshno!", "Test", MB_OK);
+        // Ждём немного, чтобы игра успела проинициализировать меню
+        Sleep(2000);
+
+        // Устанавливаем текущую страницу меню на главное меню
+        FrontEndMenuManager.m_nCurrentMenuPage = 34; // MENUPAGE_MAIN_MENU
+        FrontEndMenuManager.m_nCurrentMenuEntry = 0; // первый пункт (обычно "New Game")
+
+        char exitFlag = 0;
+        // Симулируем нажатие Enter на пункте меню
+        FrontEndMenuManager.ProcessMenuOptions(0, &exitFlag, 1);
     }
     return TRUE;
 }
